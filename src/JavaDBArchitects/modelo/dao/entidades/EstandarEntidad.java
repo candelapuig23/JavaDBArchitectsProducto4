5e60bcd1292f75af7b9460fc8d1733f70ecbe581
GerardCamell <gerard_bcn_cabo@hotmail.es>
2024-11-24 13:23:29 +0100

    Clase entidades completo

---
+++ src/JavaDBArchitects/modelo/dao/entidades/EstandarEntidad.java
@@ -0,0 +1,41 @@
+package JavaDBArchitects.modelo.dao.entidades;
+import jakarta.persistence.*;
+
+public class EstandarEntidad extends SocioEntidad {
+
+    @Column(name = "nif", nullable = false)
+    private String nif;
+
+    @ManyToOne
+    @JoinColumn(name = "id_seguro")
+    private SegurosEntidad seguro;
+
+    // Constructor por defecto
+    public EstandarEntidad() {
+    }
+
+    // Getters y Setters
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public SegurosEntidad getSeguro() {
+        return seguro;
+    }
+
+    public void setSeguro(SegurosEntidad seguro) {
+        this.seguro = seguro;
+    }
+
+    @Override
+    public String toString() {
+        return "SocioEstandarEntidad{" +
+                "nif='" + nif + '\'' +
+                ", seguro=" + seguro +
+                "} " + super.toString();
+    }
+}
