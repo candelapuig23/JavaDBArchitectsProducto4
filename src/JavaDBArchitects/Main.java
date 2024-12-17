f06c652654b2527c05bacfd2b0af6101cb5b2e6b
GerardCamell <gerard_bcn_cabo@hotmail.es>
2024-12-17 15:48:04 +0100

    Ejecución por Main

--- src/JavaDBArchitects/Main.java
+++ src/JavaDBArchitects/Main.java
@@ -1,14 +1,40 @@
 package JavaDBArchitects;
 
-import JavaDBArchitects.controlador.Controlador;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Cargar el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/JavaDBArchitects/vista/MainView.fxml"));
+        Parent root = loader.load();
+
+        // Configurar la escena
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Software centro excursionista");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        // Lanzar la aplicación JavaFX
+        launch();
+    }
+}
+
+// Main para ejecutar la aplicación por consola
+
+/*
 public class Main {
     public static void main(String[] args) {
         Controlador.iniciarAplicacion();  // Iniciar la aplicación con la inicialización de seguros y el menú
     }
 }
-
+*/
 
 
 
